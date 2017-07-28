c214c874f3b3bf8f3770718a7e9714908e5dc28d
Nooralam Shaikh <shkhnooralam6@gmail.com>
2017-07-28 18:14:48 +0530

    haris

---
+++ ping.java
@@ -0,0 +1,23 @@
+public class ping extends Applet 
+implements ActionListener{
+public void init(){
+Button b = new Button("Ping");
+b.addActionListener(this);
+add(b);
+}
+public void actionPerformed(ActionEvent e){
+
+
+String s = "";
+
+public static void main(String[] args) {
+
+String ip = "google.com";
+runSystemCommand("ping " + ip); 
+
+}
+
+}
+}
+
+
